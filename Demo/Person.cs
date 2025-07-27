badb0e1d8e1932619b9dd2c866278c29e4c7568d
Hana <hanamarwasadek2004@gmail.com>
2025-07-27 22:47:52 +0300

    demo

---
+++ Demo/Person.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal class Person
+    {
+        #region attributes
+
+        public int Id { get; set; }
+        private string? name;
+        public decimal salary { get; set; }
+
+
+
+        #endregion
+
+        #region constructor
+
+        public Person()
+        {
+            Id = default;
+            salary = default;
+            name = default;
+
+        }
+
+        public Person(int id, string? name, decimal salary)
+        {
+            Id = id;
+            this.name = name;
+            this.salary = salary;
+        }
+
+        #endregion
+
+        #region properties
+
+
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (value is not null)
+                    name = value;
+
+            }
+        }
+        #endregion
+
+        #region Methods
+
+        public override string ToString() {
+
+            return $"Id: {Id}\n Name: {name}\n Salary: {salary}";
+        }
+        #endregion
+    }      
+}
---
+++ Demo/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person person01 = new Person(1,"Hana",5000);
+
+            Person person02 = new Person();
+
+            person02.Id = 2;
+
+            person02.Name = "Joussef";
+
+            person02.salary = 3000;
+
+            Console.WriteLine(person01.ToString());
+
+            Console.WriteLine(person02);
+
+
+            
+        }
+    }
+}
