3ae3a9313266ca4235a9d46f5b393b4beecfd84f
Hana <hanamarwasadek2004@gmail.com>
2025-07-27 21:29:51 +0300

    solution of question04

--- oop#02/Program.cs
+++ oop#02/Program.cs
@@ -41,29 +41,39 @@ namespace oop_02
             #endregion
 
             #region Question03
-            Person person01 = new Person();
-            Console.WriteLine("ent first person name");
-            person01.Name = Console.ReadLine()!;
-            Console.WriteLine("ent first person age");
-            person01.Age=Convert.ToInt32(Console.ReadLine());
+            //Person person01 = new Person();
+            //Console.WriteLine("ent first person name");
+            //person01.Name = Console.ReadLine()!;
+            //Console.WriteLine("ent first person age");
+            //person01.Age=Convert.ToInt32(Console.ReadLine());
 
-            Person person02 = new Person();
-            Console.WriteLine("ent second person name");
-            person02.Name = Console.ReadLine()!;
-            Console.WriteLine("ent second person age");
-            person02.Age = Convert.ToInt32(Console.ReadLine());
+            //Person person02 = new Person();
+            //Console.WriteLine("ent second person name");
+            //person02.Name = Console.ReadLine()!;
+            //Console.WriteLine("ent second person age");
+            //person02.Age = Convert.ToInt32(Console.ReadLine());
 
-            Person person03 = new Person();
-            Console.WriteLine("ent third person name");
-            person03.Name = Console.ReadLine()!;
-            Console.WriteLine("ent third person age");
-            person03.Age = Convert.ToInt32(Console.ReadLine());
+            //Person person03 = new Person();
+            //Console.WriteLine("ent third person name");
+            //person03.Name = Console.ReadLine()!;
+            //Console.WriteLine("ent third person age");
+            //person03.Age = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine($"The oldest person is: \n {Person.GetOldestPerson(person01,person02,person03)}");
-
-
+            //Console.WriteLine($"The oldest person is: \n {Person.GetOldestPerson(person01,person02,person03)}");
 
 
+
+
+            #endregion
+
+            #region Question04
+
+            //Rectangle rectangle = new Rectangle();
+            //Console.WriteLine("Enter the width of the rectangle");
+            //rectangle.Width = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Enter the height of the rectangle");
+            //rectangle.Height = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine(rectangle);
             #endregion
         }
     }
---
+++ oop#02/Structs/Rectangle .cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_02.Structs
+{
+    internal struct Rectangle
+    {
+        #region attributes
+		
+        private double width;
+        private double height;
+        #endregion
+
+        #region Properties
+
+        public double Width
+        {
+            get { return width; }
+            set
+            {
+                if (value > 0)
+                    width = value;
+                else
+                    Console.WriteLine("The width must be positive"); ;
+            }
+        }
+
+        public double Height
+        {
+            get { return height; }
+            set
+            {
+                if (value > 0)
+                    height = value;
+                else
+                    Console.WriteLine("The height must be positive");
+            }
+        }
+
+        public double Area
+        {
+            get { return width * height; }
+        }
+        #endregion
+
+        #region Methods
+
+        public override string ToString()
+        {
+            return $"Width: {width}, Height: {height}, Area: {Area}";
+        }
+        #endregion
+
+    }
+}
