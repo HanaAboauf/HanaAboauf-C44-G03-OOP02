b9f66c93f3df8726acc11b867acf267216137046
Hana <hanamarwasadek2004@gmail.com>
2025-07-27 20:55:39 +0300

    solution of question01

--- oop#02/Program.cs
+++ oop#02/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace oop_02
+﻿using oop_02.Structs;
+
+namespace oop_02
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question01
+
+            //Person[] persons = new Person[3];
+            //persons[0] = new Person { Name = "Hana", Age = 21 };
+            //persons[1] = new Person { Name = "Joussef", Age = 19 };
+            //persons[2] = new Person { Name = "Mohammed", Age = 74 };
+
+            //foreach (Person person in persons)
+            //{
+            //    Console.WriteLine(person);
+            //}
+
+            #endregion
+
+            #region Question02
+
+
+            #endregion
         }
     }
 }
---
+++ oop#02/Structs/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_02.Structs
+{
+    internal struct Person
+    {
+        #region Properties
+
+        public string Name { set; get; }
+
+        public int Age { set; get; }
+        #endregion
+
+        #region Methods
+        public override string ToString()
+        {
+            return $"Id: {Age}, Name: {Name}";
+        } 
+        #endregion
+
+    }
+}
